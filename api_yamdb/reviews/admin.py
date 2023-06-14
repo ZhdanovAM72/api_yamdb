3d12706b416301e08cba3cd1a371f65477043fcb
Yury Zolnikov <yuzolnikov@gmail.com>
2023-06-14 07:53:21 +0300

    Added changes to admin.py

--- api_yamdb/reviews/admin.py
+++ api_yamdb/reviews/admin.py
@@ -1,3 +1,70 @@
 from django.contrib import admin
 
-# Register your models here.
+from reviews.models import Category, Comment, Genre, GenreTitle, Review, Title
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    """Настройки админ-панели категорий."""
+
+    empty_value_display = '-отсутствует-'
+    list_display = (
+        'pk',
+        'name',
+        'slug'
+    )
+    list_filter = ('name',)
+    search_fields = ('name',)
+
+
+@admin.register(Genre)
+class GenreAdmin(admin.ModelAdmin):
+    """Настройки админ-панели жанров."""
+
+    empty_value_display = '-отсутствует-'
+    list_display = (
+        'pk',
+        'name',
+        'slug'
+    )
+    list_filter = ('name',)
+    search_fields = ('name',)
+
+
+@admin.register(Title)
+class TitleAdmin(admin.ModelAdmin):
+    """Настройки админ-панели произведений."""
+
+    empty_value_display = '-отсутствует-'
+    list_display = (
+        'pk',
+        'name',
+        'year',
+        'description',
+        'category',
+        'get_genre',
+        'count_reviews',
+        'get_rating'
+    )
+    list_filter = ('name',)
+    search_fields = ('name', 'year', 'category')
+
+
+@admin.register(GenreTitle)
+class GenreTitleAdmin(admin.ModelAdmin):
+    """Класс настройки соответствия жанров и произведений."""
+
+    empty_value_display = '-отсутствует-'
+    list_display = (
+        'pk',
+        'genre',
+        'title'
+    )
+    list_filter = ('genre',)
+    search_fields = ('title',)
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Genre, GenreAdmin)
+admin.site.register(Title, TitleAdmin)
+admin.site.register(GenreTitle, GenreTitleAdmin)
