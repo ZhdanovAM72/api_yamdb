22338a34b352775e51c42a5254ec11d662118c66
Александр Жданов <canshok89@gmail.com>
2023-06-14 12:15:26 +0500

    Added permissions

---
+++ api_yamdb/api/permissions.py
@@ -0,0 +1,53 @@
+from rest_framework import permissions
+
+
+class AdminAuthorOrReadOnly(permissions.BasePermission):
+    """Админ/автор или только чтение."""
+    def has_permission(self, request, view):
+        return request.user.is_authenticated
+
+    def has_object_permission(self, request, view, obj):
+        return (
+            request.user.is_superuser
+            or request.user.is_admin
+            or obj == request.user
+        )
+
+
+class AdminOnly(permissions.BasePermission):
+    """Только админ."""
+    def has_permission(self, request, view):
+        return (
+            request.user.is_authenticated
+            and (
+                request.user.is_superuser
+                or request.user.is_admin
+                or request.user.is_staff
+            )
+        )
+
+
+class AdminOrReadOnly(permissions.BasePermission):
+    """Админ или только чтение."""
+    def has_permission(self, request, view):
+        return (
+            request.method in permissions.SAFE_METHODS
+            or (request.user.is_authenticated and request.user.is_admin)
+        )
+
+
+class AuthorOrModeratorsOrReadOnly(permissions.BasePermission):
+    """Админ/модератор/автор или только чтение."""
+    def has_permission(self, request, view):
+        return (
+            request.method in permissions.SAFE_METHODS
+            or request.user.is_authenticated
+        )
+
+    def has_object_permission(self, request, view, obj):
+        return (
+            request.method in permissions.SAFE_METHODS
+            or request.user.is_admin
+            or request.user.is_moderator
+            or obj.author == request.user
+        )
