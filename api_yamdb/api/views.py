2de556d806ae30bcab3f52b18d2e7c1c275bb9fa
ZhannaIvashova <zhannaklepikova90@gmail.com>
2023-06-15 16:38:13 +0300

    add url, views for review, comment

---
+++ api_yamdb/api/views.py
@@ -0,0 +1,55 @@
+from django.shortcuts import get_object_or_404
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+
+from api.permissions import (
+    AuthorOrModeratorsOrReadOnly,
+)
+from api.serializers import (
+    CommentSerializer,
+    ReviewSerializer,
+)
+
+from reviews.models import Review, Title
+
+
+class ReviewViewSet(ModelViewSet):
+    serializer_class = ReviewSerializer
+    permission_classes = (IsAuthenticatedOrReadOnly,
+                          AuthorOrModeratorsOrReadOnly)
+
+    def get_queryset(self):
+        title = get_object_or_404(
+            Title,
+            id=self.kwargs.get('title_id')
+        )
+        return title.reviews.all()
+
+    def perform_create(self, serializer):
+        title = get_object_or_404(
+            Title,
+            id=self.kwargs.get('title_id')
+        )
+        serializer.save(author=self.request.user, title=title)
+
+
+class CommentViewSet(ModelViewSet):
+    serializer_class = CommentSerializer
+    permission_classes = (IsAuthenticatedOrReadOnly,
+                          AuthorOrModeratorsOrReadOnly)
+
+    def get_queryset(self):
+        review = get_object_or_404(
+            Review,
+            id=self.kwargs.get('review_id'),
+            title_id=self.kwargs.get('title_id')
+        )
+        return review.comments.all()
+
+    def perform_create(self, serializer):
+        review = get_object_or_404(
+            Review,
+            id=self.kwargs.get('review_id'),
+            title_id=self.kwargs.get('title_id')
+        )
+        serializer.save(author=self.request.user, review=review)
