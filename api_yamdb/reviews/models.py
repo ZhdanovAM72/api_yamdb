82cf10951d57d3e57a1f1bdcd5a5636cd7cb7075
Yury Zolnikov <yuzolnikov@gmail.com>
2023-06-15 18:02:34 +0300

    Changed through field position

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -81,6 +81,7 @@ class Title(models.Model):
     )
     genre = models.ManyToManyField(
         Genre,
+        through='GenreTitle',
         related_name='titles',
         verbose_name='Жанр произведения',
     )
@@ -100,7 +101,6 @@ class GenreTitle(models.Model):
     genre = models.ForeignKey(
         Genre,
         on_delete=models.CASCADE,
-        through='GenreTitle',
         verbose_name='Жанр'
     )
     title = models.ForeignKey(
