5d9c5ee8576c98eb152c45ae15496be81894fa06
Александр Жданов <canshok89@gmail.com>
2023-06-16 18:38:18 +0500

    Added utils

---
+++ api_yamdb/api/utils.py
@@ -0,0 +1,22 @@
+from django.core.mail import send_mail
+from django.shortcuts import get_object_or_404
+from django.contrib.auth.tokens import default_token_generator
+
+from reviews.models import User
+
+
+def send_confirmation_code(request):
+    """Отправляет confirmation_code пользователю на почту."""
+
+    user = get_object_or_404(
+        User,
+        username=request.data.get('username'),
+    )
+    user.confirmation_code = default_token_generator.make_token(user)
+    user.save()
+    send_mail(
+        'Код для получения токена к API',
+        f'Код подтверждения {user.confirmation_code}',
+        'token_API@yamdb.ru',
+        [request.data.get('email')],
+    )
